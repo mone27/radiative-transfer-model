56699891c3e506a7020b6e7506a939ed821855da
mone27 <massaro.simone.it@gmail.com>
2020-12-22 20:19:04 +0100

    Fixed longwave model

--- longwave.R
+++ longwave.R
@@ -3,65 +3,66 @@
 ## NEED TO TEST THIS with the Matlab code
 
 longwave_radiation <- function(inputs, params){
-  
+  ## --
+  lw_soil_emit <- params$em_soil * params$sigma * inputs$t_soil^4
+  lw_leaf_emit <- params$em_leaf * params$sigma  * inputs$t_leaf^4
+
   ## equations 14.134
-  lw_down_trans <- function(x) 
+  lw_down_trans <- function(x)
     inputs$lw_sky * (1 - params$em_leaf * (1-exp(-params$Kd * x)))
   lw_down_emit <- function(x)
-    params$em_leaf * params$sigma * inputs$t_leaf^4 *(1-exp(-params$Kd * x))
-  
+    lw_leaf_emit *(1-exp(-params$Kd * x))
+
   lw_down <- function(LAI) lw_down_emit(LAI) + lw_down_trans(LAI)
-  
-  ## --
-  lw_up_soil <- params$em_soil * params$sigma * inputs$t_soil^4
-  
+
+
   ## equations 14.135
-  
+
   lw_up_trans <- function(x) {
-    l_upw_soil * (1 - params$em_leaf * (1-exp(-params$Kd * (inputs$LAI - x))))
+    lw_soil_emit * (1 - params$em_leaf * (1-exp(-params$Kd * (inputs$LAI - x))))
   }
-  
-  lw_leaf_emit <- params$em_leaf * params$sigma * inputs$t_leaf^4
+
   lw_up_emit <- function(x)
-    l_leaf_emit * (1-exp(-params$Kd * (inputs$LAI - x)))
-  
+    lw_leaf_emit * (1-exp(-params$Kd * (inputs$LAI - x)))
+
+  lw_up <- function (x) lw_up_trans(x) + lw_up_emit(x)
   ## equation 14.137
   perc_abs <-  1-exp(-params$Kd * inputs$LAI) # amount absorbed
-  lc <- params$em_leaf * (inputs$lw_sky + lw_up_soil) * perc_abs
-         - 2 * lw_leaf_emit * perc_abs
-  
+  lc <- perc_abs * (params$em_leaf * (inputs$lw_sky + lw_soil_emit)
+         - 2 * lw_leaf_emit)
+
   ## equation 14.138
-  lc_soil <- lw_down(inputs$LAI) - lw_up_soil
-  
-  return(list(lc = lc, lc_soil = lc_soil))
+  lg<- lw_down(inputs$LAI) - lw_soil_emit # Lw adboserbed by the soil
+
+
+  ### --- Sunlit and shaded leaves ---
+
+  # Sunlit Eq. 14.140
+
+  lc_sun <-
+  (
+      (
+        (params$em_leaf * (inputs$lw_sky - params$sigma * inputs$t_leaf ^ 4 ) * params$Kd )
+        / (params$Kd + params$Kb)
+        * (1 - exp(-(params$Kd + params$Kb) * inputs$LAI))
+      )
+      +
+      (
+        (params$em_leaf *(lw_soil_emit - params$sigma * inputs$t_leaf ^ 4 ) * params$Kd)
+        / (params$Kd - params$Kb)
+        * (exp(-params$Kb * inputs$LAI)  - exp(-params$Kd * inputs$LAI))
+      )
+  )
+
+  # Shaded Eq. 14.141
+
+  lc_sha <- lc - lc_sun
+
+  return(list(lc = lc,                        # Lw radiation absorbed by the canopy
+              lg = lg,                        # Lw radiation absorbed by the soil
+              lc_sun = lc_sun,                # Lw radiation absorbed by the sunlit canopy
+              lc_sha = lc_sha,                # Lw radiation absorbed by the shaded canopy
+              l_up = lw_up(0),             # Lw emitted into the sky
+              l_down = lw_down(inputs$LAI)   # Lw reaching the soil
+  ))
 }
-
-
-params <- list(
-  rho_leaf = 0.10,                    # Leaf reflectance
-  tau_leaf = 0.05,                    # Leaf transmittance
-  Kb = 0.58,                          # Direct beam extinction coefficient
-  Kd = 0.70,                          # Diffuse extinction coefficient
-  beta = 0.54,                        # Upscatter parameter for diffuse radiation
-  beta0 = 0.46,                       # Upscatter parameter for direct beam radiation
-  clump_OMEGA = 0.75,                 # Clumping coefficient 
-  alb_soil_b = 0.1,                   # Soil albedo (direct)  
-  alb_soil_d = 0.1,                   # Soil albedo (diffuse)
-  em_leaf = .98,                      # Emissivity leaves
-  em_soil = 1.00,                     # Emissivity soil
-  sigma = 5.67e-08                    # Stefan-Boltzan costant (TODO: Unit)
-  
-)
-
-params$omega_leaf = params$rho_leaf + params$tau_leaf   # Leaf scattering coefficient
-
-
-inputs <- list(
-  sw_sky_b = 0.8,             # Short wave (sw) direct beam radiation (W m^-2)
-  sw_sky_d = 0.2,             # Short wave (sw) diffuse radiation (W m^-2)
-  lw_sky = 400,               # Long wave (sw) diffuse radiation (W m^-2)
-  t_leaf = 273.15 + 25,       # Canopy temperature (K)
-  t_soil =273.15 + 20,        # Soil temperature (K)
-  LAI = 6                     # Leaf Area Index
-)
-longwave_radiation(inputs, params)
